03538ccc0a56312e73dac4596e771a42db46720e
danielrios <danielrios.exe@hotmail.com>
2025-10-17 07:53:30 -0600

    Feedback de proyecto

---
+++ Feedback profe.rb
@@ -0,0 +1,10 @@
+1 Nombres en dimensiones y facts
+2 Agregar counts de partidas
+3 Agregar Union con dimension para linkear los registros "no encontrado" venedores y clientes
+4 Errores de nulos en vendedoes
+7 Dimension medio de embarque
+
+-- Tableau
+5 Titulo en dashboard
+6 Filtros visibles en dashboard
+8 Robustecer los análisis
